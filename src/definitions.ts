508fcb4cf72c89db373fb37ca7c38de03e5824b2
twestrick <twestrick@mediagrid.us>
2024-07-18 14:51:00 -0400

    Add web files and update package.json

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,74 @@
+interface AudioPlayerDefaultParams {
+  audioId: string;
+}
+
+interface AudioPlayerPrepareParams extends AudioPlayerDefaultParams {
+  audioSource: string;
+  friendlyTitle: string;
+  useForNotification: boolean;
+  isBackgroundMusic?: boolean;
+  loop?: boolean;
+}
+
+interface AudioPlayerListenerParams {
+  audioId: string;
+}
+
+interface AudioPlayerListenerResult {
+  callbackId: string;
+}
+
 export interface AudioPlayerPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  create(params: AudioPlayerPrepareParams): Promise<{ success: boolean }>;
+  initialize(params: AudioPlayerDefaultParams): Promise<{ success: boolean }>;
+
+  changeAudioSource(
+    params: AudioPlayerDefaultParams & { source: string },
+  ): Promise<void>;
+
+  getDuration(params: AudioPlayerDefaultParams): Promise<{ duration: number }>;
+  getCurrentTime(
+    params: AudioPlayerDefaultParams,
+  ): Promise<{ currentTime: number }>;
+
+  play(params: AudioPlayerDefaultParams): Promise<void>;
+  pause(params: AudioPlayerDefaultParams): Promise<void>;
+  seek(
+    params: AudioPlayerDefaultParams & { timeInSeconds: number },
+  ): Promise<void>;
+  stop(params: AudioPlayerDefaultParams): Promise<void>;
+
+  setVolume(
+    params: AudioPlayerDefaultParams & { volume: number },
+  ): Promise<void>;
+  setRate(params: AudioPlayerDefaultParams & { rate: number }): Promise<void>;
+
+  isPlaying(params: AudioPlayerDefaultParams): Promise<{ isPlaying: boolean }>;
+
+  destroy(params: AudioPlayerDefaultParams): Promise<void>;
+
+  onAppGainsFocus(
+    params: AudioPlayerListenerParams,
+    callback: () => void,
+  ): Promise<AudioPlayerListenerResult>;
+
+  onAppLosesFocus(
+    params: AudioPlayerListenerParams,
+    callback: () => void,
+  ): Promise<AudioPlayerListenerResult>;
+
+  onAudioReady(
+    params: AudioPlayerListenerParams,
+    callback: () => void,
+  ): Promise<AudioPlayerListenerResult>;
+
+  onAudioEnd(
+    params: AudioPlayerListenerParams,
+    callback: () => void,
+  ): Promise<AudioPlayerListenerResult>;
+
+  onPlaybackStatusChange(
+    params: AudioPlayerListenerParams,
+    callback: (result: { status: 'playing' | 'paused' | 'stopped' }) => void,
+  ): Promise<AudioPlayerListenerResult>;
 }
