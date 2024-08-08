d1c92f0a19117b1591aa662c1af87b62f82f0a74
twestrick <twestrick@mediagrid.us>
2024-08-08 17:18:47 -0400

    Add NowPlayingIcon to the docs

--- src/definitions.ts
+++ src/definitions.ts
@@ -41,7 +41,7 @@ export interface AudioPlayerPrepareParams extends AudioPlayerDefaultParams {
    * you would put the image in your `public` folder and the build process will copy to `dist`
    * which in turn will be copied to the Android assets by Capacitor.
    *
-   * A PNG is the best option with square dimensions. 1200px x 1200px is a good option.
+   * A PNG is the best option with square dimensions. 1200 x 1200px is a good option.
    *
    * @example A public web source: https://example.com/artwork.png
    * @example An in-app source: images/artwork.png
