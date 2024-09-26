fba48656df84ddbd0d8079f8e729c12b54b27896
Tom Westrick <twestrick@mediagrid.us>
2024-09-26 19:05:17 -0400

    Formatting

--- ios/Sources/AudioPlayerPlugin/AudioSources.swift
+++ ios/Sources/AudioPlayerPlugin/AudioSources.swift
@@ -1,39 +1,40 @@
 public class AudioSources {
-    private var audioSources: [String : AudioSource] = [:]
-    
+    private var audioSources: [String: AudioSource] = [:]
+
     public func get(sourceId: String) -> AudioSource? {
         return audioSources[sourceId]
     }
-    
+
     public func add(source: AudioSource) throws {
-        if (exists(source: source)) {
+        if exists(source: source) {
             throw AudioPlayerError.sourceAlreadyExists
         }
-        
+
         audioSources[source.assetId] = source
     }
-    
+
     public func remove(sourceId: String) -> Bool {
-        
+
     }
-    
+
     public func exists(source: AudioSource) -> Bool {
         return exists(sourceId: source.assetId)
     }
-    
+
     public func exists(sourceId: String) -> Bool {
         return audioSources[sourceId] != nil
     }
-    
+
     public func hasNotification() -> Bool {
-        
+
     }
-    
+
     public func forNotification() -> AudioSource? {
-        return audioSources.first(where: <#T##((key: String, value: AudioSource)) throws -> Bool#>)
+        return audioSources.first(
+            where: <#T##((key: String, value: AudioSource)) throws -> Bool#>)
     }
-    
-    public func count()->Int {
+
+    public func count() -> Int {
         return audioSources.count
     }
 }
