f1e4ba1cb6c67d5b1c68ef953627961efbf33523
William Phillips <william.phillips21@outlook.com>
2024-12-27 18:33:38 -0500

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "MediagridCapacitorNativeAudio",
+            name: "SoundsCapacitorNativeAudio",
             targets: ["AudioPlayerPlugin"])
     ],
     dependencies: [
