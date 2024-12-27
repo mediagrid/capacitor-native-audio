77956ce81880cf99df8a38825c0b10a1f6ea5564
William Phillips <william.phillips21@outlook.com>
2024-12-27 18:35:48 -0500

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MediagridCapacitorNativeAudio",
+    name: "SoundsCapacitorNativeAudio",
     platforms: [.iOS(.v13)],
     products: [
         .library(
