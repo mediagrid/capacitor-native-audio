1f610f72977b075849dd3bab53c7346e6ce235ee
twestrick <twestrick@mediagrid.us>
2025-06-18 09:45:10 -0400

    Change example livereload port to Vite's default 5173

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
     appName: 'example',
     webDir: 'dist',
     server: {
-        url: 'http://localhost:3000',
+        url: 'http://localhost:5173',
         cleartext: true,
     },
 };
